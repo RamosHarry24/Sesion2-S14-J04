4729637b0f96ee2ef98c38ae535beb741892ebb2
ryosangels24@gmail.com <ryosangels24@gmail.com>
2022-11-16 14:25:51 -0500

    tareaS14

---
+++ jscript.js
@@ -0,0 +1,31 @@
+//EJERCICIO 1
+//  let number1= +prompt('Ingrese la cantidad de varones')
+//  let number2= +prompt('Ingrese la cantidad de mujeres')
+
+//  let sumtotal = number1 + number2;
+
+//  alert ('La suma total es de:  ' +(number1 + number2));
+
+//  alert ('el porcentaje de varones es de :  ' +( number1 * 100 / sumtotal + "%"));
+
+//  alert ('el porcentaje de mujeres es de:  ' +( number2 * 100 / sumtotal + "%"));
+
+//EJERCICIO 2
+
+// let inversion = +prompt("Ingrese el monto de la inversion");
+
+// console.log(`Av. La mar: ${inversion * 0.35}`);
+// console.log(`Av.Abancay: ${inversion * 0.25} `);
+// console.log(`Av. 28 de Julio: ${inversion * 0.1}`);
+// console.log(`Av. Aviacion: ${inversion * 0.15}`);
+// console.log(`Av. Tacna: ${inversion * 0.15}`);
+
+//EJERCICIO 3
+
+// let general = +prompt("Cuantas entradas generales se vendieron")
+// let mayores = +prompt("Cuantas entradas para mayores de 65 años se vendieron")
+// let menores = +prompt("Cuantas entradas para menores de 18 años se vendieron")
+
+// console.log(`Entrada general ${general * 150} soles`)
+// console.log(`Entrada mayores ${mayores * 50} soles`)
+// console.log(`Entrada menores ${menores * 80} soles`)

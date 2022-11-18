09b0831090ef2e208dc25c2a8811bbab2f1aa010
ryosangels24@gmail.com <ryosangels24@gmail.com>
2022-11-18 15:26:05 -0500

    S16

---
+++ S16script.js
@@ -0,0 +1,46 @@
+//EJERCICIO 1
+
+// let multipli = function(x,y) {
+//     return x * y 
+// }
+// let expon = function(x,y) {
+//     return Math.pow(x,y)
+// }
+// let suma = function(x,y) {
+//     return x + y 
+// }
+// let resta = function(x,y) {
+//     return x - y
+// }
+
+// var result = multipli(5,2)
+// alert(result)
+// result = suma(3,4)
+// alert(result)
+// result = resta(9,7)
+// alert(result)
+
+//EJERCICIO 2
+
+// let pasarFarenheit = (grado) => {
+//     return (Farenheit = grado * 1.8 + 32)
+// }
+// var result = pasarFarenheit(150)
+// alert(result)
+
+// let pasarCelsius = (grado) => {
+//     return (Celsius = (grado - 32) * (5/9))
+// }
+// var result = pasarCelsius(150)
+// alert(result.toFixed(2))
+
+
+//EJERCICIO 3
+
+// let a = +prompt("Ingrese un número:")
+// let b = +prompt("Ingrese otro número más:")
+
+// function suma(a,b){
+//     return a + b
+// }
+// alert(`La suma de ${a} + ${b} es ${suma(a,b)}`)
